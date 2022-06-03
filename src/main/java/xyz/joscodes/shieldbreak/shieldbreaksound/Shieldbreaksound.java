537d937a9887a07f02c22d775b6b1e323c7ec2e6
Jos <43651265+JosTheDude@users.noreply.github.com>
2022-06-03 19:40:07 -0400

    Added Config.yml

--- src/main/java/xyz/joscodes/shieldbreak/shieldbreaksound/Shieldbreaksound.java
+++ src/main/java/xyz/joscodes/shieldbreak/shieldbreaksound/Shieldbreaksound.java
@@ -1,6 +1,7 @@
 package xyz.joscodes.shieldbreak.shieldbreaksound;
 
 import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,9 +11,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Shieldbreaksound extends JavaPlugin implements Listener {
+	final FileConfiguration config = this.getConfig();
+
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, (Plugin) this);
+		config.addDefault("noise", "ENTITY_WITHER_SKELETON_HURT");
+		config.options().copyDefaults(true);
+		saveConfig();
 	}
 
 	@Override
@@ -25,8 +31,15 @@ public class Shieldbreaksound extends JavaPlugin implements Listener {
 			return;
 		final Player player = (Player) event.getEntity();
 		final Player damager = (Player) event.getDamager();
-		if (damager.getEquipment().getItemInMainHand().getType().toString().toLowerCase().contains("axe") && player.isBlocking() && (int) event.getFinalDamage() == 0)
-			damager.playSound(damager.getLocation(), Sound.ENTITY_WITHER_SKELETON_HURT, 1.0F, 1.0F);
-		// yeah im so lazy i didnt add a config to the noise, fight me
+		if (damager.getEquipment().getItemInMainHand().getType().toString().toLowerCase().contains("axe") && player.isBlocking() && (int) event.getFinalDamage() == 0) {
+			try {
+				damager.playSound(damager.getLocation(), Sound.valueOf(getConfig().getString("noise")), 5, 5);
+				//Everything is OK!
+			} catch (final Exception e) {
+				damager.playSound(damager.getLocation(), Sound.ENTITY_WITHER_SKELETON_HURT, 5, 5);
+				System.out.println("The entered sound in the config is not a valid sound, so ShieldBreakNoise played the default one! Please enter a valid value in the config.");
+				//Error, entered sound does not exist / missing sound from config
+			}
+		}
 	}
 }
